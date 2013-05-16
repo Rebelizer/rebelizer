4a3e11e3e9d2d99a919897d1a1e35ee788314f95
Demetrius Johnson <contact@demetriusj.com>
2013-05-16 13:02:31 -0700

    Give birth to Rebelizer

---
+++ Contributing.md
@@ -0,0 +1,20 @@
+## Submitting issues
+
+If you have questions about how to use AngularJS, please direct these to
+[Issues][issues] or [StackOverflow][stackoverflow].
+
+### Guidelines
+
+* Search the archive first, it's likely that your question was already answered.
+* A live example demonstrating your problem or question, will get an answer faster.
+* If you get help, help others. Good karma rulez!
+
+If your issue appears to be a bug, and hasn't been reported, open a new issue.
+Help us to maximize the effort we can spend fixing issues and adding new
+features, by not reporting duplicate issues.
+
+## Contributing to Source Code
+
+
+[issues]: https://github.com/rebelizer/rebelizer/issues
+[stackoverflow]: http://stackoverflow.com/questions/tagged/angularjs
---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    dox: {
+      options: {
+        title: "Rebelizer API Docs"
+      },
+      files: {
+        src: ['lib/**/*.js'],
+          dest: 'doc'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: false,
+        eqnull: true
+      },
+      all: ['Gruntfile.js', 'index.js', 'lib/**/*.js', 'test/**/*.js']
+    },
+    changelog: {
+      options: {
+        dest: 'Changelog.md'
+      }
+    },
+    simplemocha: {
+      options: {
+        ignoreLeaks: false,
+        reporter: 'list'
+      },
+
+      all: { src: ['test/**/*.test.js'] }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-dox');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-conventional-changelog');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+
+  // Default task.
+  grunt.registerTask('test', ['simplemocha']);
+  grunt.registerTask('default', ['jshint', 'dox', 'test']);
+};
